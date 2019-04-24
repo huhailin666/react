799c41826d149931d650a74f3cf2975f5e34e286
xiaohai <857919293@qq.com>
2019-04-24 13:28:32 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <h1 className="App-link">
           Learn React
-        </a>
+        </h1>
       </header>
+      <button onClick={()=>alert('Hi')}>点我</button>  
     </div>
   );
 }
